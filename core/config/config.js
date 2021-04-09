3dab68b2e6de4ce9b3249d27a46434142f37554a
YaroslavYedyn <clavik.edin@gmail.com>
2021-04-09 14:04:16 +0300

    initial program settings

---
+++ core/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 5050,
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost/rolique',
+    ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || 'http://localhost:4200;http://localhost:3000',
+};
