c9bd90afeff59ae68b54fbcd0d838d5b621f9024
YaroslavYedyn <clavik.edin@gmail.com>
2021-04-11 15:28:57 +0300

    check refresh token

--- core/error/error.messages.js
+++ core/error/error.messages.js
@@ -3,4 +3,15 @@ module.exports = {
     BAD_REQUEST: {
         customCode: 4000
     },
+    NO_TOKEN: {
+        customCode: 4004
+    },
+    // UNAUTHORIZED
+    WRONG_TOKEN: {
+        customCode: 4011
+    },
+    // NOT FOUND
+    USER_NOT_FOUND: {
+        customCode: 4042
+    },
 };
