afcf104f9e3e80654b0bee23299e363bffb6c201
xsv7y <taras.kutsyk.pm.2019@lpnu.ua>
2021-04-14 13:29:49 +0300

    _

--- core/routes/user/user.router.js
+++ core/routes/user/user.router.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { userController } = require('../../controllers');
 const { userMiddleware, fileMiddleware, authMiddleware } = require('../../middlewares');
 
-// TODO: check roles & tokens
 router.get('/', userController.getUsers);
 router.post('/',
     authMiddleware.checkAccessToken,
