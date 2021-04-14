a30a1dd6a6deb14985faafdb8792d4e83f931702
YaroslavYedyn <clavik.edin@gmail.com>
2021-04-14 09:39:30 +0300

    cron-job delete old tokens

--- core/config/config.js
+++ core/config/config.js
@@ -2,5 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 5050,
     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost/rolique',
     ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || 'http://localhost:4200;http://localhost:3000',
-    PASSWORD_SALT_VALUE: 10
+
+    PASSWORD_SALT_VALUE: 10,
+    CRON_JOB_PERIOD: process.env.CRON_JOB_PERIOD || '0 1 * * *',
 };
