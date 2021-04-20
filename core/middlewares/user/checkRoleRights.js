8bf3a74a747c88fad8e580b9566976a28a85d23d
YaroslavYedyn <clavik.edin@gmail.com>
2021-04-21 02:03:51 +0300

    cloudinary connected

--- core/middlewares/user/checkRoleRights.js
+++ core/middlewares/user/checkRoleRights.js
@@ -13,17 +13,14 @@ module.exports = (req, res, next) => {
             body: { role: roleToCreate = ROLES.EMPLOYEE },
         } = req;
 
-        console.log(req.user);
-
+        console.log(roleToCreate);
+        console.log(clientRole);
         const isClientAllowed = rolesMatcher(clientRole, roleToCreate);
-
-        console.log(isClientAllowed);
         if (!isClientAllowed) {
             throw new ErrorHandler(errorCodes.FORBIDDEN,
                 errorMessages.UNAUTHORIZED_ROLE.customCode, errorMessages.UNAUTHORIZED_ROLE.message);
         }
 
-        console.log(req.user);
         next();
     } catch (e) {
         next(e);
