1e29e636bf641549c2b3de67e763c68437cff448
YaroslavYedyn <clavik.edin@gmail.com>
2021-06-15 14:42:34 +0300

    youtube channel type C fix

--- core/services/youtube.service.js
+++ core/services/youtube.service.js
@@ -7,14 +7,8 @@ const { config: { YOUTUBE_API_KEY } } = require('../config');
 module.exports = {
     getYouTubeVideosByUrl: async (url) => {
         try {
-            const channelType = url.split('/')[url.split('/').length - 2];
-
-            const id = url.split('/')
-                .pop();
-
             const sorting = (items) => {
                 const videoUrl = [];
-
                 const username = items.pop().snippet.channelTitle;
 
                 items.map(value => videoUrl.push({
@@ -23,46 +17,52 @@ module.exports = {
                     preview: value.snippet.thumbnails.high.url,
                     social_name: 'youtube'
                 }));
-
                 return {
                     videoUrl,
                     username
                 };
             };
-            if (channelType !== 'channel') {
-                const username = url.split('/')
-                    .pop();
 
+            const search = async (params) => {
+                console.log({ name: 'vaas', ...params });
+                const { data } = await service.search.list({
+                    key: YOUTUBE_API_KEY,
+                    part: 'snippet',
+                    ...params,
+                    maxResults: '12',
+                    order: 'date',
+                    type: 'video'
+                });
+                return sorting(data.items);
+            };
+
+            const channelType = url.split('/')[url.split('/').length - 2];
+            const username = url.split('/')
+                .pop();
+
+            if (channelType === 'user') {
                 const channelDetails = await service.channels.list({
                     key: YOUTUBE_API_KEY,
                     part: 'snippet,contentDetails,statistics',
                     forUsername: username,
                 });
+                const channelId = channelDetails.data.items[0] && channelDetails.data.items[0].id;
 
-                const channelId = channelDetails.data.items[0].id;
-
-                const { data } = await service.search.list({
+                return search({ channelId });
+            }
+            if (channelType === 'c') {
+                const { data: { items } } = await service.search.list({
                     key: YOUTUBE_API_KEY,
                     part: 'snippet',
-                    channelId,
-                    maxResults: '16',
-                    order: 'date',
-                    type: 'video'
+                    type: 'channel',
+                    q: username,
+                    maxResults: 50,
+                    order: 'relevance',
                 });
-
-                console.log(data);
-                return sorting(data.items);
+                const id = items[0].id.channelId ? items[0].id.channelId : items[0].snippet.channelId;
+                return search({ channelId: id });
             }
-            const { data: { items } } = await service.search.list({
-                key: YOUTUBE_API_KEY,
-                part: 'snippet',
-                channelId: id,
-                maxResults: '16',
-                order: 'date',
-                type: 'video'
-            });
-
-            return sorting(items);
+            return search({ channelId: username });
         } catch (e) {
             console.log(e);
             return e;
